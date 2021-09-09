ad1ca9f9790bd2eacbd7e9c1945df796c6449646
DMA8 <41307001+DMA8@users.noreply.github.com>
2021-09-10 01:10:47 +0300

    refactored

--- To-Do List/task/todolist/todolist.py
+++ To-Do List/task/todolist/todolist.py
@@ -3,9 +3,8 @@ import datetime as dt
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# 1) create an engine
+
 engine = sql.create_engine('sqlite:///todo.db?check_same_thread=False')
-# 2) create a db
 Base = declarative_base()
 
 
@@ -21,128 +20,107 @@ class Table(Base):
 
 # creating the table in DB
 Base.metadata.create_all(engine)  # It generates SQL query like CREATE TABLE.....
-
 # to access the DB we need to create a session
 Session = sessionmaker(bind=engine)
 session = Session()
 
 
-# to fill a db table we need to create an object of class 'Table' and put it in session.add(obj)
-
-# HOW TO CREATE A DATETIMEOBJ:
-# dt_field = datetime.datetime.strptime(the date like '01-24-2020', the mask like '%m-%d-%Y').date()
-
-# new_task = Table(task='task', deadline=dt.datetime.today())
-# session.add(new_task)
-# session.commit()
+MAIN_WINDOW = "1) Today's tasks\n2) Week's tasks\n3) All tasks\n4) Missed tasks\n5) Add task\n6) Delete task\n0) Exit"
 
 
-def print_main_window():
-    print("1) Today's tasks\n2) Week's tasks\n3) All tasks\n4) Missed tasks\n5) Add task\n6) Delete task\n0) Exit")
-
-
-def add_task():
-    tsk = input()
-    new_task = Table(task=tsk, deadline=enter_deadline())
+def add_task(tsk: str, deadline) -> None:
+    new_task = Table(task=tsk, deadline=dt.datetime.strptime(deadline, '%Y-%m-%d'))
     session.add(new_task)
     session.commit()
 
 
-def print_all_tasks():
-    rows = session.query(Table).order_by(Table.deadline).all()
-    if not rows:
-        print('Nothing to do!')
-        print()
-        return
-    for i in rows:
-        print(str(i.id) + '.', i.task + '.', i.deadline.day, i.deadline.strftime('%b'), sep=' ')
-    print()
+def delete_tasks(task_id=None):
+    tasks = get_tasks('all')
+    if not task_id:
+        print('Choose the number of the task you want to delete:')
+        print_tasks(tasks, 'all')
+        task_id = int(input())
+    session.query(Table).filter(Table.id == task_id).delete()
 
 
-def print_today():
-    rows = session.query(Table).filter(Table.deadline == dt.datetime.today().date()).all()
-    print('Today ', dt.datetime.today().day, ' ', dt.datetime.today().strftime('%b'), ':', sep='')
-    if not rows:
-        print('Nothing to do!')
-        print()
-        return
-    for i in range(len(rows)):
-        print(i+1, '. ', rows[i], sep='')
-    print()
+def get_tasks(period: str):
+    tasks = []
+    if period == 'all':
+        tasks = session.query(Table).order_by(Table.deadline).all()
+    elif period == 'today':
+        tasks = session.query(Table).filter(Table.deadline == dt.datetime.today().date()).all()
+    elif period == 'week':
+        tasks = session.query(Table).filter(sql.and_(Table.deadline >= dt.datetime.now().date(), \
+                                                     Table.deadline <= dt.datetime.now().date() + dt.timedelta(
+                                                         days=7))).all()
+    elif period == 'missed':
+        tasks = session.query(Table).filter(Table.deadline < dt.datetime.today().date()).order_by(
+            Table.deadline).all()
+
+    return tasks
 
 
-def week_tasks():
-    days = []
-    for i in range(0, 7, 1):
-        days.append(dt.datetime.today().date() + dt.timedelta(i))
-    for day in days:
-        tasks = session.query(Table).filter(Table.deadline == day).all()
-        print(week_days[day.weekday()], day.day, day.strftime('%b'), ':')
+def print_tasks(tasks: list, period):
+    """tasks only in  <class '__main__.Table'> """
+    if period == 'today':
         if not tasks:
-            print('There is nothing to do!')
-            print()
-            continue
-        for j in range(len(tasks)):
-            print(j+1, '. ', tasks[j], sep='')
+            print('Nothing to do!\n')
+            return
+        for i in range(len(tasks)):
+            print(i + 1, '. ', tasks[i], sep='')
+    elif period == 'week':
         print()
-    print()
-
-week_days = {0: 'Monday', 1: 'Tuesday', 2: 'Wednesday', 3: 'Thursday', 4: 'Friday', 5: 'Saturday', 6: 'Sunday'}
-
-def missed_tasks():
-    losed_deadlines = session.query(Table).filter(Table.deadline < dt.datetime.today().date()).order_by(Table.deadline).all()
-    print('Missed tasks:')
-    if not losed_deadlines:
-        print('Nothing is missed!')
-        return
-    for i in range(len(losed_deadlines)):
-        print(i+1, '. ', losed_deadlines[i],'. ',losed_deadlines[i].deadline.day, ' ',losed_deadlines[i].deadline.strftime('%b'), sep='')
-    print()
-def delete_tasks():
-    rows = session.query(Table).order_by(Table.deadline).all()
-    print('Choose the number of the task you want to delete:')
-    if not rows:
-        print('Nothing to delete!')
+        days = [dt.datetime.today().date() + dt.timedelta(i) for i in range(7)]
+        for day in days:
+            match = None
+            print(day.strftime('%A'), day.day, day.strftime('%b'), ':')
+            for task in range(len(tasks)):
+                match = tasks[task].deadline == day
+                if match:
+                    print(task + 1, '. ', tasks[task], sep='')
+            else:
+                print('There is nothing to do!\n')
+                continue
         print()
-        return
-    for i in rows:
-        print(str(i.id) + '.', i.task + '.', i.deadline.day, i.deadline.strftime('%b'), sep=' ')
-    print()
-    a = int(input())
-    session.query(Table).filter(Table.id == a).delete()
-    print('The task has been deleted!')
-
-def enter_deadline():
-    deadline = input('Enter deadline')
-    deadline = dt.datetime.strptime(deadline, '%Y-%m-%d')
-    return deadline
-
-
-while True:
-    session.commit()
-    print_main_window()
-    client_command = input()
-    print()
-    if client_command == '1':
-        # Today's tasks
-        print_today()
-    elif client_command == '2':
-        week_tasks()
-    elif client_command == '3':
-        # All tasks
-        print_all_tasks()
-    elif client_command == '4':
-        missed_tasks()
-    elif client_command == '5':
-        # Add task
-        print('Enter task')
-        add_task()
-        print('The task has been added')
-    elif client_command == '6':
-        delete_tasks()
-    elif client_command == '0':
-        print('Bye!')
-        break
-    else:
-        print('Incorrect command! Try again!')
+    elif period == 'missed':
+        if not tasks:
+            print('Nothing is missed!')
+            return
+        for i in range(len(tasks)):
+            print(i + 1, '. ', tasks[i], '. ', tasks[i].deadline.day, ' ', \
+                  tasks[i].deadline.strftime('%b'), sep='')
         print()
+    elif period == 'all':
+        if not tasks:
+            print('There is nothing to do!\n')
+            return
+        for i in tasks:
+            print(str(i.id) + '.', i.task + '.', i.deadline.day, i.deadline.strftime('%b'), sep=' ')
+        print()
+
+
+def interface():
+    while True:
+        session.commit()
+        print(MAIN_WINDOW)
+        client_command = input('\n')
+        time_code = {'1': 'today', '2': 'week', '3': 'all', '4': 'missed'}
+        manipulate_code = {'5': 'add', '6': 'delete', '0': 'exit'}
+        if client_command in time_code:
+            print_tasks(get_tasks(time_code[client_command]), time_code[client_command])
+        elif client_command in manipulate_code:
+            if manipulate_code[client_command] == 'add':
+                add_task(input('Enter the task '), input('Enter the deadline in format "YYYY-M-D"'))
+                print('The task has been added!')
+            elif manipulate_code[client_command] == 'delete':
+                delete_tasks()
+                print('The task has been deleted!')
+            elif manipulate_code[client_command] == 'exit':
+                print('Bye!')
+                break
+        else:
+            print('Incorrect command! Try again!\n')
+
+
+if __name__ == '__main__':
+    interface()
